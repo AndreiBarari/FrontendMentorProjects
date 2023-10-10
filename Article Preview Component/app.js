8d9dc905e9448701587ad3aa7184393a15e43f41
Andrei Barari <b.andrei91@yahoo.com>
2023-10-10 15:26:08 +0300

    style adjustments

--- Article Preview Component/app.js
+++ Article Preview Component/app.js
@@ -2,14 +2,11 @@ const btn = document.querySelector(".share-btn");
 const share = document.querySelector(".icons");
 
 share.style.visibility = "hidden"; // Set the initial state to "hidden"
-
+console.log(btn);
 btn.addEventListener("click", () => {
   if (share.style.visibility === "hidden") {
     share.style.visibility = "visible";
-    btn.style.backgroundColor = "var(--desaturated-dark-blue)";
-    // btn.style.fill = "#fff";
   } else {
     share.style.visibility = "hidden";
-    btn.style.backgroundColor = "var(--light-gray-blue)";
   }
 });
